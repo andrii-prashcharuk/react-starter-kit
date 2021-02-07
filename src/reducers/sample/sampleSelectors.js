27ce467fa2c910a8f25b206ad8021cc6c1c1d36d
Andrii Prashcharuk <monext09@gmail.com>
2021-02-07 11:20:05 +0200

    fix: flowtype issue

--- src/reducers/sample/sampleSelectors.js
+++ src/reducers/sample/sampleSelectors.js
@@ -1,6 +1,6 @@
 // @flow
 import type { State, DataItem } from '../../types';
 
-export const getSampleData = (state: State): DataItem[] => state.sample.data;
+export const getSampleData = (state: State): DataItem[] | null => state.sample.data;
 export const isSampleFetching = (state: State): boolean => state.sample.isFetching;
 export const getSampleError = (state: State): string | null => state.sample.error;
